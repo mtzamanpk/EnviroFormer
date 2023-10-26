530a5ed6b5d34e6a0d4ee382f190c85604ae5f94
Muhammad Zaman <mtzamanpk@gmail.com>
2023-10-26 13:18:17 -0600

    changed movement variables
    added acceleration, adjusted jump velocity, and speed, slowed down to improve game flow

--- src/player.gd
+++ src/player.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const ACCELERATION = 400 
+const JUMP_VELOCITY = -275.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -12,7 +13,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+ 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -21,7 +22,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, SPEED * direction, ACCELERATION * delta) 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 

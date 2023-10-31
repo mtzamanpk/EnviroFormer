8ea126c68313346d67870a253b17df3aa7d9ff04
Muhammad Zaman <mtzamanpk@gmail.com>
2023-10-31 09:36:48 -0600

    added character sprite, and animations for it
    added the sprite, with its frames for idle stance, run animation frames, and jump frame, along with adding the function to use them

--- src/player.gd
+++ src/player.gd
@@ -8,12 +8,15 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 func _physics_process(delta):
 	apply_gravity(delta)
 	handle_jump()
 	var input_axis = Input.get_axis("ui_left", "ui_right")
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
+	update_animtions(input_axis)
 	move_and_slide()
 
 func apply_gravity(delta):
@@ -35,3 +38,12 @@ func apply_friction(input_axis, delta):
 func handle_acceleration(input_axis, delta):
 	if input_axis:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta) 
+
+func update_animtions(input_axis):
+	if input_axis != 0:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")

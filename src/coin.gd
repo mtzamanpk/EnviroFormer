a31d2daed0c1444a32ac3307d417f313fff68d9b
Muhammad Zaman <mtzamanpk@gmail.com>
2023-11-08 13:06:25 -0600

    adding level completed pop up

--- src/coin.gd
+++ src/coin.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 func _on_body_entered(body):
-	queue_free()
+	queue_free()	
 	var coins = get_tree().get_nodes_in_group("Coins")
 	if coins.size() == 1:
-		print ("Level Completed")
+		Events.level_compelted.emit()
--- src/world.gd
+++ src/world.gd
@@ -1,14 +1,9 @@
 extends Node2D
-
+ 
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	RenderingServer.set_default_clear_color(Color.SKY_BLUE)
-	
+	RenderingServer.set_default_clear_color(Color.SKY_BLUE)	
 	polygon_2d.polygon = collision_polygon_2d.polygon
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass

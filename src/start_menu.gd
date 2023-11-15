eaf12767403edf8b3fef04ed2c359bb5aea617af
Muhammad Zaman <mtzamanpk@gmail.com>
2023-11-15 10:11:58 -0600

    added start menu

---
+++ src/start_menu.gd
@@ -0,0 +1,10 @@
+extends CenterContainer
+
+func _ready():
+	pass
+
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
